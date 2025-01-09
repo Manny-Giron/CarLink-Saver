df31548b727b53ee1c67df7f14345075f6b98e5c
Emmanuel Giron <115903418+Manny-Giron@users.noreply.github.com>
2025-01-09 10:33:18 -0500

    Minor structure details update

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
 import LogoutButton from '../components/logoutButton';
 
 
 
 const Dashboard = () => {
     return <main>
-        <div>
-            <h1 style={styles.title}>Welcome to the dashboard</h1>
-            <p> Working on building...</p>
-            <LogoutButton />
+        <h1 style={styles.title}>Welcome to the Dashboard</h1>
+        <div className='MainContainer'>
+            <div className='MainBody'>
+
+            </div>
+            <div className="LeftBar">
+                <div style={styles.leftMiniContainers}>
+                    <p>Cont</p>
+                </div>
+                <LogoutButton />
+            </div>
         </div>
     </main>
 };
 
 const styles = {
     title: {
-        border: '2px solid black',
+        paddingBottom: '2rem',
+        borderBottom: '2px solid black',
         borderRadius: '0.5rem',
+    },
+    leftBar: {
+        borderRight: '1px solid rgb(0,0,0, 0.9)',
+        display: 'flex',
+        justifyContent: 'center',
+        itemsAlign: 'center',
+    },
+    leftMiniContainers: {
+        textAlign: 'center',
+        border: '2px solid black',
+        borderRadius: '2px',
+        padding: '0.1rem',
     },
 }
 
